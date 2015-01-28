2ffcd5b702c5a94498fa54f58500db7fe3071be9
Kyle DeFauw <kyle.defauw@gmail.com>
2015-01-28 15:46:50 -0700

    create about and terms page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 root 'welcome#index'
+get '/about', to: 'about#index'
+get '/terms', to: 'terms#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
