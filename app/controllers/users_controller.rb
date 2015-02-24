e808decd54da3542e65603cb88edfe7909085982
Kyle DeFauw <kyle.defauw@gmail.com>
2015-02-24 14:27:58 -0700

    Change variable name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    product = User.find(params[:id])
-    product.destroy
+    user = User.find(params[:id])
+    user.destroy
     flash[:notice] = "User was successfully deleted"
     redirect_to users_path
   end
