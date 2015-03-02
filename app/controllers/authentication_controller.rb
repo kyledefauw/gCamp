9bd213f11c52e9e6fa2d928454bc3fc0fc65d44a
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-02 16:56:55 -0700

    refactor flash message to application layout folder

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,24 @@
+class AuthenticationController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully logged in"
+      redirect_to roots_path
+    else
+      flash[:notice] = "You have successfully logged in"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end
