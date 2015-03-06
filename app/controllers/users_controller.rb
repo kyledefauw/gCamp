b83e6540b3176f64c99d3cf6a6c2e5dcb327658a
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-06 15:36:03 -0700

    Update params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confimration)
   end
 
 end
