0ec9f810d8e49993b781f59294b31cb7568abe7c
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-05 22:27:49 -0700

    Add validations for sign up and sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, :last_name, :email, :presence => true
   validates :email, :uniqueness => true
+  validates :password, :presence => true,
+                     :confirmation => true,
+                     :length => {:within => 6..40},
+                     :on => :create
+  validates :password, :confirmation => true,
+                       :length => {:within => 6..40},
+                       :allow_blank => true,
+                       :on => :update
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
