73463ed817eb87d55d294e619340286b707fe6ac
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-07 15:25:13 -0700

    Refactor tasks controller to have nested projects routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,33 +1,34 @@
 class TasksController < ApplicationController
-
+  before_action :find_and_set_project
   before_filter :ensure_signed_in
 
   def index
-    @tasks = Task.all
+    @tasks = @project.tasks
+    @name = @project.name
   end
 
   def new
-    @task = Task.new
+    @task = @project.tasks.new
   end
 
   def create
-    @task = Task.new(task_params)
+    @task =  @project.tasks.new(task_params)
 
     if @task.save
       flash[:notice] = "Task was successfully created"
 
-      redirect_to task_path(@task)
+      redirect_to project_task_path(@project, @task)
     else
       render :new
     end
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = @project.tasks.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = @project.tasks.find(params[:id])
   end
 
   def update
@@ -35,22 +36,27 @@ class TasksController < ApplicationController
 
     if @task.update(task_params)
       flash[:notice] = "Task was successfully updated"
-      redirect_to task_path(@task)
+      redirect_to project_task_path(@project, @task)
     else
       render :edit
     end
   end
 
   def destroy
-    Task.destroy(params[:id])
+    task = @project.tasks.find(params[:id])
+    task.destroy
     flash[:notice] = "Task was successfully deleted"
-    redirect_to tasks_path
+    redirect_to project_tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:description, :complete, :date)
+    params.require(:task).permit(:description, :complete, :date, :project_id)
+  end
+
+  def find_and_set_project
+    @project = Project.find(params[:project_id])
   end
 
 end
