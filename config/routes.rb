1b333282d429fed7c61b214d768be90678a31bd8
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-07 15:24:45 -0700

    Nest tasks routes in projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
 
-root 'welcome#index'
-get '/about', to: 'about#index'
-get '/terms', to: 'terms#index'
-get '/faq', to: 'common_questions#index'
+  root 'welcome#index'
+  get '/about', to: 'about#index'
+  get '/terms', to: 'terms#index'
+  get '/faq', to: 'common_questions#index'
+  resources :users
 
-resources :tasks
-resources :users
-resources :projects
+  resources :projects do
+    resources :tasks
+  end
 
-get 'sign-up', to: 'registrations#new'
-post 'sign-up', to: 'registrations#create'
-get 'sign-out', to: 'authentication#destroy'
-get 'sign-in', to: 'authentication#new'
-post 'sign-in', to: 'authentication#create'
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+  get 'sign-out', to: 'authentication#destroy'
+  get 'sign-in', to: 'authentication#new'
+  post 'sign-in', to: 'authentication#create'
 
 end
