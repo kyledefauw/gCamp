9172ba2a5d91c986b517555ea8ed052d86cc06bd
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-07 15:24:25 -0700

    Add project_id column to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302230308) do
+ActiveRecord::Schema.define(version: 20150307191835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150302230308) do
     t.datetime "updated_at",  null: false
     t.boolean  "complete"
     t.date     "date"
+    t.integer  "project_id"
   end
 
   create_table "users", force: :cascade do |t|
