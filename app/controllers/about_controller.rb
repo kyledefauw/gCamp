a76698146a6995daee0ca375792e98752982ff29
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-26 14:27:32 -0600

    Add count to the number of projects, tasks, members, users, comments

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < PublicController
   def index
+    @project = Project.all.count
+    @task = Task.all.count
+    @membership = Membership.all.count
+    @user = User.all.count
+    @comment = Comment.all.count
   end
 end
