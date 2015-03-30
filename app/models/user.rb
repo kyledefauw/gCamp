79642b2ed1a58b8e02d2d8258c4bc0fca31c1773
Kyle DeFauw <kyle.defauw@gmail.com>
2015-03-30 15:14:07 -0600

    add dependent destroy to user/memberships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
   has_many :projects, through: :memberships
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
 
   def full_name
     "#{first_name} #{last_name}"
